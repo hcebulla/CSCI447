41cfe3b6504cb93a08885934df2272066bee8e63
Hannah Cebulla <hannahmcebulla@gmail.com>
2020-09-28 16:14:10 -0600

    data handling

--- Project2/main.py
+++ Project2/main.py
@@ -2,14 +2,44 @@ import numpy
 import csv
 import random
 
+forestfires = open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/forestfires.csv')
+glass = open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/glass.csv')
+abalone = open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/abalone.csv')
+machine = open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/machine.csv')
+vote = open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/vote.csv')
+segmentation = open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/segmentation.csv')
+
 #preprocess data set
 def process(filename):
-	with open(r'/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/forestfires.csv') as csvfile:
+	with open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/forestfires.csv', 'r') as csvfile:
 		lines = csv.reader(csvfile)
 		for row in lines:
 			print(', '.join(row))
 
-#split data into training and test sets
-def splitData(filename, split, trainingSet=[], testSet=[])
-	with open(
+#randomly split data into training and test sets
+def splitData(filename, split, trainingSet=[], testSet=[]):
+	with open('/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/forestfires.csv', 'r') as csvfile:
+		lines = csv.reader(csvfile)
+		dataset = list(lines)
+		for i in range(len(dataset) - 1):
+			#for j in range(4):
+				#dataset[i][j] = dataset[i][j]
+			if random.random < split:
+				trainingSet.append(dataset[i])
+			else:
+				testSet.append(dataset[i])
+
+process(forestfires)
+process(glass)
+process(abalone)
+process(machine)
+process(vote)
+process(segmentation)
+
+trainingSet=[]
+testSet=[]
+
+splitData(forestfires, 10, trainingSet, testSet)
+print(repr(len(trainingSet)))
+print(repr(len(testSet)))
 
