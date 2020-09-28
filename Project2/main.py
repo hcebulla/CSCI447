a164735ab3927b27325d825854f9330418c099f0
Hannah Cebulla <hannahmcebulla@gmail.com>
2020-09-27 19:24:12 -0600

    data processing

--- Project2/main.py
+++ Project2/main.py
@@ -1,4 +1,15 @@
-input numpy
+import numpy
+import csv
+import random
 
-def main():
+#preprocess data set
+def process(filename):
+	with open(r'/Users/hannahcebulla/Desktop/CSCI447/CSCI447/Project2/forestfires.csv') as csvfile:
+		lines = csv.reader(csvfile)
+		for row in lines:
+			print(', '.join(row))
+
+#split data into training and test sets
+def splitData(filename, split, trainingSet=[], testSet=[])
+	with open(
 
