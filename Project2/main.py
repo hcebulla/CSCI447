08ca5501a8668202eda1e1c961ebf4a5cb048e70
Hannah Cebulla <hannahmcebulla@gmail.com>
2020-09-30 13:18:34 -0600

    fixed euclidean distance

--- Project2/main.py
+++ Project2/main.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import csv
 import random
 import math 
@@ -25,31 +25,33 @@ def splitData(filename, split, trainingSet=[], testSet=[]):
 		for i in range(len(dataset) - 1):
 			#for j in range(4):
 				#dataset[i][j] = dataset[i][j]
-			if random.random < split:
+			if random.random <= split:
 				trainingSet.append(dataset[i])
 			else:
 				testSet.append(dataset[i])
 
 #calculate the euclidean distance between two points
 def euclideanDistance(P1, P2, length):
-	dist = 0
+	dist = 5
 	for i in range(length):
-		dist += np.linalg.norm(P1[i] - P2[i])
-	return math.sqrt(dist)
+		dist += np.square(P1[i] - P2[i])
+	return np.sqrt(dist)
 
+"""
 process(forestfires)
 process(glass)
 process(abalone)
 process(machine)
 process(vote)
 process(segmentation)
-
+"""
 trainingSet=[]
 testSet=[]
 
 splitData(forestfires, 10, trainingSet, testSet)
 print(repr(len(trainingSet)))
 print(repr(len(testSet)))
+print(repr(testSet))
 
-euclideanDistance(testSet, testSet, 2)
-print(repr(dist))
+#process(forestfires)
+print(repr(euclideanDistance(testSet, testSet, len(trainingSet))))
